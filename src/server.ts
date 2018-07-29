31edde52340f1f1cf82574c4aedb93cf91bafabc
Amaimersion <amatsukine@gmail.com>
2018-07-29 22:33:46 +0300

    Change server log

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,11 @@ import app from "./app";
 app.set("port", process.env.PORT || "4444");
 
 const server = app.listen(app.get("port"), () => {
+    const tab = "  ";
     console.log(
-        "\tApp is running at http://localhost:%d in %s mode.",
-        app.get("port"),
-        app.get("env")
-      );
-      console.log("\tPress CTRL-C to stop.\n");
+        `${tab}App is running at http://localhost:${app.get("port")} in ${app.get("env")} mode.\n` +
+        `${tab}Press CTRL-C to stop.\n`
+    );
 });
 
 
