44ebf34ed324064f880aacc9c1a2a050f3d48191
Amaimersion <amatsukine@gmail.com>
2018-08-03 12:42:11 +0300

    Add start key in main py script

--- src/markov-chain/main.py
+++ src/markov-chain/main.py
@@ -1,17 +1,17 @@
 import sys
 from markov_chain import MarkovChain, extensions
 
-
 def main():
     chain = MarkovChain()
     chain.chain = extensions.file.json.read("./src/markov-chain/generated-chains/ru/my-favorites-3-window.json")
 
-    #print("Ready")
-
     while True:
         arg = input()
-        print(chain.generate())
 
+        if (arg):
+            print(chain.generate(start=arg))
+        else:
+            print(chain.generate())
 
 if __name__ == "__main__":
     main()
